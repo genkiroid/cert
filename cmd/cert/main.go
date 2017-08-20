8757a1e99476184b347fe85db1aae4ae004f2312
genkiroid <gamurai@gmail.com>
2017-08-20 18:18:25 +0900

    Add \n

--- cmd/cert/main.go
+++ cmd/cert/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	c, err := cert.NewCerts(os.Args)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
 	fmt.Print(c.Markdown())
