edc98bb9bdd2369f0dce771e676fa06d311e08bf
genkiroid <gamurai@gmail.com>
2018-04-10 23:17:21 +0900

    Add testable examples

---
+++ cert_example_test.go
@@ -0,0 +1,44 @@
+package cert
+
+import (
+	"fmt"
+)
+
+func ExampleDefaultFormat() {
+	UTC = true
+	stubCert()
+	certs, _ := NewCerts([]string{"example.com"})
+
+	fmt.Printf("%s", certs)
+	// Output:
+	// DomainName: example.com
+	// IP:         127.0.0.1
+	// Issuer:     CA for test
+	// NotBefore:  2016-12-31 15:00:00 +0000 UTC
+	// NotAfter:   2017-12-31 15:00:00 +0000 UTC
+	// CommonName: example.com
+	// SANs:       [example.com www.example.com]
+	// Error:
+}
+
+func ExampleMarkDownFormat() {
+	UTC = true
+	stubCert()
+	certs, _ := NewCerts([]string{"example.com"})
+
+	fmt.Printf("%s", certs.Markdown())
+	// Output:
+	// DomainName | IP | Issuer | NotBefore | NotAfter | CN | SANs | Error
+	// --- | --- | --- | --- | --- | --- | --- | ---
+	// example.com | 127.0.0.1 | CA for test | 2016-12-31 15:00:00 +0000 UTC | 2017-12-31 15:00:00 +0000 UTC | example.com | example.com<br/>www.example.com<br/> |
+}
+
+func ExampleJSONFormat() {
+	UTC = true
+	stubCert()
+	certs, _ := NewCerts([]string{"example.com"})
+
+	fmt.Printf("%s", certs.JSON())
+	// Output:
+	// [{"domainName":"example.com","ip":"127.0.0.1","issuer":"CA for test","commonName":"example.com","sans":["example.com","www.example.com"],"notBefore":"2016-12-31 15:00:00 +0000 UTC","notAfter":"2017-12-31 15:00:00 +0000 UTC","error":""}]
+}
